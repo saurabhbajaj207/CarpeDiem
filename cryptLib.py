8fba342ba8f23a4571a25224558920ffb54cd69d
saurabh207 <saurabhsbajaj@gmail.com>
2017-09-29 02:27:22 +0530

    added encrypt and decrypt functions

--- cryptLib.py
+++ cryptLib.py
@@ -1,1 +1,23 @@
-#Contains all crytography related functions
+# Contains all crytography related functions
+
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+
+
+def getKey(password):
+    return SHA256.new(password).digest()
+
+
+def encrypt(plainText, password):
+    key = getKey(password)
+    ctr = Counter.new(128)
+    cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
+    return cipher.encrypt(plainText)
+
+
+def decrypt(cipherText, password):
+    key = getKey(password)
+    ctr = Counter.new(128)
+    cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
+    return cipher.decrypt(cipherText)
